a1fe63950bc41b1bc958b9c4ef0071a6f95c6cf7
Kareem Eltwargy <83376355+KareemEltwargy@users.noreply.github.com>
2024-02-13 17:31:53 +0200

    CustomerClass

--- ClassLibrary/Class1.cs
+++ ClassLibrary/Class1.cs
@@ -1,7 +1,22 @@
-﻿namespace ClassLibrary
-{
-    public class Class1
-    {
+﻿using System.ComponentModel.DataAnnotations;
 
+namespace ClassLibrary
+{
+    public class Customer
+    {
+        [Key]
+        public int ID { get; set; }
+        [DataType(DataType.ImageUrl)]
+        public string? ImageUrl { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        public Gender Gender { get; set; }
     }
+    public enum Gender { Male, Female}
 }
