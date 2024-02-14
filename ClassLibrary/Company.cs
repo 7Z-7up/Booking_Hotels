0ad48df784a074ca656840cbcc9d2fa5d1736fe8
Mahmoud Taha <121981608+7Z-7up@users.noreply.github.com>
2024-02-14 20:46:02 +0200

    Create Company.cs

---
+++ ClassLibrary/Company.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ClassLibrary
+{
+    [Table("Company")]
+    internal class Company
+    {
+       
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal TotalProfits { get; set; }
+
+        public virtual List<Hotel> Hotels { get; set; }
+    }
+
+
+}
