e0b3db9d18f3bd63985db4c6762a3d75994cb8f3
mostafa500030 <71846308+mostafa500030@users.noreply.github.com>
2024-02-14 18:39:16 +0200

    AddHotel

---
+++ ClassLibrary/Hotel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClassLibrary
+{
+    public class Hotel
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public double Rate { get; set; }
+        [Required]
+        public string Address { get; set; }
+
+        public List<string>? Images { get; set; }
+
+        //public bool AdditionalServices { get; set; }
+
+        [Required]
+        public int RoomId { get; set; }
+
+        public virtual List<Room>? Rooms { get; set; }
+    }
+}
